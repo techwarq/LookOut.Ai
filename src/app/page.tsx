e938df5f45d73538828192b924f5b54420fcc5ad
techwarq <102250604+techwarq@users.noreply.github.com>
2024-08-02 23:00:41 +0530

    landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import Navbar from "@/components/globals/navbar";
+import Image from "next/image";
+import {Compare} from "@/components/ui/compare"
+import { ContainerScroll } from "@/components/ui/container-scoll-animation";
+import { Button } from "@/components/ui/button";
+import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+import { motion, MotionValue, useSpring, useScroll, useTransform } from 'framer-motion';
+import { useState } from "react";
+import React from "react";
+import ClientTweetCard from "@/components/globals/ClientTweetCard";
+export default function Home() {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+ 
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
+  return (
+    <main className="  flex items-center justify-center flex-col">
+      <Navbar />
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center flex-col">
+                <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Coming Soon
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Know The Players Of Your Market
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+
+      <section className="mt-96 pt-40" ref={ref}>
+      <GoogleGeminiEffect
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+      />
+
+      
+
+      </section>
+
+      <section className="mt-96">
+      <ClientTweetCard id="1812827725977334244" />;
+
+      </section>
+    </main>
+  );
+}
